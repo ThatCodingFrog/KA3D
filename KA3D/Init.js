51b8fb5de760d51a5233eed54125fbe659661831
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-10-06 16:06:01 -0600

    Update Init.js

--- KA3D/Init.js
+++ KA3D/Init.js
@@ -1,10 +1,18 @@
 import { Scene } from "../src/three/src/scenes/Scene.js";
+import { WebGLRenderer } from "../src/three/src/renderers/WebGLRenderer.js";
+import { EventTarget, World } from "../src/physics/cannon-es.js";
 
 let scene, camera, renderer, world;
 
 function Init() {
   if(window.parent !== undefined) cancelAnimationFrame(window.parent.raf);
   scene = new Scene();
+  renderer = new WebGLRenderer({
+    antialias: true,
+    preserveDrawingBuffer: true
+  });
+
+  world = new World();
 }
 
 export { Init, scene, camera, renderer, world }
