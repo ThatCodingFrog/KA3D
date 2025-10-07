9d7f6d09a4611b83ecd01656335b056751b967dc
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-10-07 08:06:41 -0600

    Update setMaterial.js

--- KA3D/setMaterial.js
+++ KA3D/setMaterial.js
@@ -1,1 +1,36 @@
 import { material } from "./Init.js";
+
+import { MeshBasicMaterial } from "../src/three/src/materials/MeshBasicMaterial.js";
+import { MeshNormalMaterial } from "../src/three/src/materials/MeshNormalMaterial.js";
+import { MeshPhongMaterial } from "../src/three/src/materials/MeshPhongMaterial.js";
+import { MeshPhysicalMaterial } from "../src/three/src/materials/MeshPhysicalMaterial.js";
+import { MeshToonMaterial } from "../src/three/src/materials/MeshToonMaterial.js";
+import { error } from "../src/three/src/utils.js";
+
+const 
+  NORMAL = "Normal",
+  BASIC = "Basic",
+  PHONG = "Phong",
+  PHYSICAL = "Physical",
+  TOON = "Toon";
+
+function setMaterial(materialType = "Normal", attributes = {}) {
+  switch(materialType) {
+    case NORMAL:
+      material = new MeshNormalMaterial(attributes);
+      break;
+    case BASIC:
+      material = new MeshBasicMaterial(attributes);
+      break;
+    case PHONG:
+      material = new MeshPhongMaterial(attributes);
+      break;
+    case PHYSICAL:
+      material = new MeshPhysicalMaterial(attributes);
+      break;
+    case TOON:
+      material = new MeshToonMaterial(attributes);
+      break;
+    default:
+      error(materialType, "is not a valid material type or currently supported in KA3D");
+  }
