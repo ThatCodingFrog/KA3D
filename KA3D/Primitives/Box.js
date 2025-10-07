f25fa757b1d2256d7f6f81335f26b9ebe4fbcaa1
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-10-07 13:14:27 -0600

    Update Box.js

--- KA3D/Primitives/Box.js
+++ KA3D/Primitives/Box.js
@@ -2,13 +2,15 @@ import { BoxGeometry } from "../src/three/src/geometries/BoxGeometry.js";
 import { Mesh } from "../src/three/src/objects/Mesh.js";
 import { Box, Body, Vec3 } from "../src/physics/cannon-es.js";
 import { scene, world, material } from "../Init.js";
-import { physMeshes } from "../Render.js";
+import { physMeshes, threeMeshes } from "../Render.js";
 
 class Box {
   constructor(w = 100, h = 100, d = 100, mass = 1) {
     this.shape = new Mesh(new BoxGeometry(w, h, d), material);
     scene.add(this.shape);
 
+    threeMeshes.push(this.shape);
+    
     this._physShape = new Box(new Vec3(w/2, h/2, d/2))
     this.physShape = new Body({mass: mass, shape: this._physShape});
     world.addBody(this.physShape);
