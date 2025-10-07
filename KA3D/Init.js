d4cdc6f380f4230f65e7378ce513f0638cb98a1d
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-10-06 18:32:18 -0600

    Update Init.js

--- KA3D/Init.js
+++ KA3D/Init.js
@@ -1,5 +1,7 @@
 import { Scene } from "../src/three/src/scenes/Scene.js";
 import { WebGLRenderer } from "../src/three/src/renderers/WebGLRenderer.js";
+import { PerspectiveCamera } from "../src/three/src/cameras/PerspectiveCamera.js";
+
 import { EventTarget, World } from "../src/physics/cannon-es.js";
 
 let scene, camera, renderer, world;
@@ -7,6 +9,7 @@ let scene, camera, renderer, world;
 function Init() {
   if(window.parent !== undefined) cancelAnimationFrame(window.parent.raf);
   scene = new Scene();
+  camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
   renderer = new WebGLRenderer({
     antialias: true,
     preserveDrawingBuffer: true
