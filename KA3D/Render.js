5d6473c61798707565b9e1f24e077d20103ea32d
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-10-07 13:13:54 -0600

    Update Render.js

--- KA3D/Render.js
+++ KA3D/Render.js
@@ -1,5 +1,12 @@
+import { scene, camera, renderer, world } from "./Init.js";
+
+let physMeshes = [], threeMeshes = [];
+
 function Render() {
-  
+  for(var i = 0; i < threeMeshes.length; i++) {
+    threeMeshes[i].position.copy(physMeshes[i].position);
+    threeMeshes[i].quaternion.copy(physMeshes[i].quaternion);
+  }
 }
 
-export { Render }
+export { Render, physMeshes, threeMeshes }
