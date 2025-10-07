dfa0384b2b9f9a2483c74661b514f572f8b8132a
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-10-07 17:08:36 -0600

    Update Render.js

--- KA3D/Render.js
+++ KA3D/Render.js
@@ -3,6 +3,13 @@ import { scene, camera, renderer, world } from "./Init.js";
 let physMeshes = [], threeMeshes = [];
 
 function Render() {
+  if(window.parent !== undefined) window.parent.raf = requestAnimationFrame(Render);
+  else requestAnimationFrame(Render);
+
+  world.step( 1 / 60 );
+
+  renderer.render(scene, camera);
+  
   for(var i = 0; i < threeMeshes.length; i++) {
     threeMeshes[i].position.copy(physMeshes[i].position);
     threeMeshes[i].quaternion.copy(physMeshes[i].quaternion);
